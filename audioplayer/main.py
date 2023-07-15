b4f18e6e71ca743c01ceb07976fc557faeb0b7d6
selfdestroying <max.f99@yandex.ru>
2023-07-15 16:45:49 +0300

    Add shuffle tracks function Add repeat one track function Add saving playlist to json file and load playlist from it

--- audioplayer/main.py
+++ audioplayer/main.py
@@ -1,3 +1,5 @@
+import json
+import random
 import time
 
 import flet as ft  # type: ignore
@@ -8,10 +10,12 @@ MIN_WIDTH = 500
 
 def main(page: ft.Page):
     # Page size settings
+    page.window_prevent_close = True
     page.window_width = MIN_WIDTH
     page.window_height = MIN_HEIGHT
     page.window_resizable = False
-    track_list: dict[str, str] = {}
+    track_list_to_play: dict[str, str] = {}
+    track_list_to_save: dict[str, str] = {}
     audio: ft.Audio
 
     # Formatting time in 00:00 format
@@ -22,9 +26,43 @@ def main(page: ft.Page):
         return formatted_time
 
     def find_key():
-        for key, val in track_list.items():
+        for key, val in track_list_to_play.items():
             if val == audio.src:
                 return key
+
+    def create_json(e):
+        if e.data == 'close':
+            with open('playlist.json', 'w') as f:
+                json.dump(track_list_to_save, f, indent=4)
+                print(json.dumps(track_list_to_save, indent=4))
+            page.window_destroy()
+
+    def create_track_list():
+        nonlocal track_list_to_play, track_list_to_save
+        try:
+            with open('playlist.json', 'r') as f:
+                track_list_to_play = json.load(f)
+                track_list_to_save = track_list_to_play
+            display_tracks()
+        except FileNotFoundError:
+            print('File not found')
+
+    def display_tracks():
+        new_track_list = []
+        for i in track_list_to_play.keys():
+            new_track_list.append(
+                ft.ElevatedButton(i, on_click=configure_track))
+        track_list_view.controls = new_track_list
+        page.update()
+
+    def shuffle_track_list(e):
+        nonlocal track_list_to_play
+        print(track_list_to_play)
+        tracks = list(track_list_to_play.keys())
+        random.shuffle(tracks)
+        track_list_to_play = dict([(m, track_list_to_play[m]) for m in tracks])
+        print(track_list_to_play)
+        configure_track(file_name=list(track_list_to_play.keys())[0])
 
     def change_volume(e):
         nonlocal audio
@@ -49,21 +87,24 @@ def main(page: ft.Page):
     def play_next_track(filename=None):
         nonlocal audio
         if filename:
-            track = list(track_list.keys())
-            if track.index(filename) + 1 > len(track) - 1:
-                next_track = track[0]
+            track = list(track_list_to_play.keys())
+            if repeat.value:
+                configure_track(file_name=filename)
             else:
-                next_track = track[track.index(filename) + 1]
-            time.sleep(.1)
-            configure_track(file_path=track_list[next_track], file_name=next_track)
+                if track.index(filename) + 1 > len(track) - 1:
+                    next_track = track[0]
+                else:
+                    next_track = track[track.index(filename) + 1]
+                time.sleep(.1)
+                configure_track(file_name=next_track)
 
     def play_prev_track(filename=None):
         nonlocal audio
         if filename:
-            track = list(track_list.keys())
+            track = list(track_list_to_play.keys())
             next_track = track[track.index(filename) - 1]
             time.sleep(.1)
-            configure_track(file_path=track_list[next_track], file_name=next_track)
+            configure_track(file_name=next_track)
 
     def check_state(e, filename):
         if e.data == 'completed':
@@ -77,15 +118,14 @@ def main(page: ft.Page):
         current_time.value = format_time(0)
         page.update()
 
-    def configure_track(e=None, file_path=None, file_name=None):
+    def configure_track(e=None, file_name=None):
         nonlocal audio
         if e is not None:
             file_name = e.control.text
-            file_path = track_list[file_name]
         if type(ft.Audio()) in [type(i) for i in page.overlay]:
             page.overlay.pop()
         audio = ft.Audio(
-            src=file_path,
+            src=track_list_to_play[file_name],
             autoplay=False,
             volume=volume_slider.value / 100,
             balance=0,
@@ -107,19 +147,19 @@ def main(page: ft.Page):
         page.update()
 
     def load_track(e):
-        nonlocal track_list
+        nonlocal track_list_to_play
 
         for j in e.files:
             file_name = j.name
             file_path = j.path
             if file_name not in [i.text for i in track_list_view.controls]:
-                track_list[file_name] = file_path
-                print(track_list)
-                track_list_view.controls.append(
-                    ft.ElevatedButton(file_name, on_click=configure_track))
+                track_list_to_play[file_name] = file_path
+                track_list_to_save[file_name] = file_path
+                print(track_list_to_play)
             else:
                 print('Track always in tracklist')
             page.update()
+            display_tracks()
 
     # File picker
     file_picker = ft.FilePicker(on_result=load_track)
@@ -136,6 +176,9 @@ def main(page: ft.Page):
         on_click=lambda e: file_picker.pick_files(
             allow_multiple=True,)
     )
+    shuffle_button = ft.IconButton(ft.icons.SHUFFLE, on_click=shuffle_track_list)
+    # Repeat checkbox
+    repeat = ft.Checkbox(label='Repeat')
     # Time text
     track_name = ft.Text(value='-TRACK NAME-', text_align=ft.TextAlign.CENTER)
     current_time = ft.Text(value='--:--')
@@ -146,7 +189,8 @@ def main(page: ft.Page):
         alignment=ft.MainAxisAlignment.SPACE_BETWEEN
     )
     # Progress bar
-    track_length_bar = ft.Slider(min=0, value=0, on_change=lambda e: audio.seek(position_milliseconds=int(float(e.data))))
+    track_length_bar = ft.Slider(min=0, value=0,
+                                 on_change=lambda e: audio.seek(position_milliseconds=int(float(e.data))))
     # Volume slider
     volume_slider = ft.Slider(min=0,
                               max=100,
@@ -159,22 +203,27 @@ def main(page: ft.Page):
     autoplay = ft.Checkbox(label='Autoplay')
     # Track list
     track_list_view = ft.ListView(expand=True, spacing=2)
+
+    create_track_list()
     # Add elems to interface
     page.controls.append(
         ft.Column(
             [ft.Row([track_name], alignment=ft.MainAxisAlignment.CENTER), time_row, track_length_bar, buttons_row,
              ft.Row(
                  [
-                     add_track_button,
                      autoplay,
+                     repeat,
+                     shuffle_button,
                      volume_slider
                  ],
                  alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-             )],
+             ),
+             add_track_button],
             alignment=ft.MainAxisAlignment.CENTER),
     )
     page.controls.append(track_list_view)
     page.overlay.append(file_picker)
+    page.on_window_event = create_json
     page.update()
 
 
